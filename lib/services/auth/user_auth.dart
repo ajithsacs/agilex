9a391ea3b27d79b3e2a874cc4bcd834603d7ed39
AjithS <ajiths@agilexcyber.in>
2022-07-28 18:25:43 +0530

    authuser file added bool

--- lib/services/auth/user_auth.dart
+++ lib/services/auth/user_auth.dart
@@ -1,11 +1,10 @@
 import 'package:firebase_auth/firebase_auth.dart' show User;
 import 'package:flutter/cupertino.dart';
+
 @immutable
-class AuthUser
-{ 
+class AuthUser {
   final bool isEmailverifyed;
-  const AuthUser(this.isEmailverifyed);
-  factory AuthUser.fromfirebase(User user)=>AuthUser(user.emailVerified);
+  const AuthUser({required this.isEmailverifyed});
+  factory AuthUser.fromfirebase(User user) =>
+      AuthUser(isEmailverifyed: user.emailVerified);
 }
-
-
